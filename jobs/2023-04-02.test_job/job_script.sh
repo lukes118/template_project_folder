7febb6f0e5c08660746bdb710349565181555503
Luke Staszewski (mac) <lstaszewski@pks.mpg.de>
2023-04-02 17:21:36 +0200

    tested on cluster and mac

---
+++ jobs/2023-04-02.test_job/job_script.sh
@@ -0,0 +1,3 @@
+for i in {0..2}; do
+julia /Users/lstaszewski/Research/Projects/template_project_folder/code_example.jl /Users/lstaszewski/Research/Projects/template_project_folder/jobs/2023-04-02.test_job/config/config_$i.toml
+done
